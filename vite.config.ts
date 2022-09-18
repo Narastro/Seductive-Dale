1a4c18f69ba955a4129bdba183d821a59f47c68d
dale-vand <dale.vand@kakaoenterprise.com>
2022-09-18 18:36:07 +0900

    :rocket: init!

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
+import {resolve} from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '',
+  resolve: {
+    alias: {
+      src: resolve('src')
+    }
+  },
+  plugins: [svelte()]
+})
